71521fe19d1d5f67331afbb53be5168b23ed233d
kimsung jun <koke007@hanmail.net>
2016-09-06 20:14:26 +0900

    First commit
    프로젝트 생성

---
+++ MakeQuiz/MakeQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MakeQuiz
+//
+//  Created by kimsung jun on 2016. 9. 6..
+//  Copyright © 2016년 kimsung jun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
